39fb0c62de7e1e811e895335992999c2a6fb9289
qimengchen1 <1508201428@qq.com>
2024-07-25 14:51:20 +0800

    Update layout-hm.vue

--- docs/examples/container/layout-hm.vue
+++ docs/examples/container/layout-hm.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="common-layout">
     <el-container>
-      <el-header>Header</el-header>
+      <el-header>Header222</el-header>
       <el-main>Main</el-main>
     </el-container>
   </div>
